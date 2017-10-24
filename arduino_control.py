e9871afe98a7e7f5c66f17f7d791a94c5d27d9dd
Alejocram <alejocram@gmail.com>
2017-10-23 20:41:57 -0500

    Fixing to connect with arduino and test the servo comands

--- arduino_control.py
+++ arduino_control.py
@@ -3,15 +3,15 @@ from time import sleep
 import serial
 
 class ArduinoControl:
-    ARDUINO_PORT = "/dev/ttyACM0";
-    RATE = 115200
+    ARDUINO_PORT = "/dev/ttyACM0"
+    RATE = 9600
 
     def __init__(self):
         self.arduino = serial.Serial(self.ARDUINO_PORT, self.RATE)
 
     # Establishes the connection between Arduino and Python before starting the processing.
     def establish_arduino_connection(self):
-        self.arduino.readline()
+        # self.arduino.readline()
         print("Arduino connection established.")
 
     # Sends the given parameter to Arduino and returns it response.
--- test_arduino_control.py
+++ test_arduino_control.py
@@ -1,5 +1,6 @@
 from arduino_control import ArduinoControl
 import sys
+from time import sleep
 
 
 arduino_control = ArduinoControl()
@@ -9,5 +10,9 @@ while True:
     try:
         response = arduino_control.write_to_arduino('l')
         print(response)
+        sleep(1)
+        response = arduino_control.write_to_arduino('r')
+        print(response)
+        sleep(1)
     except:
         sys.exit(1)
